9cf9d736dd47736dc55bd1373b2c282e8e5e6171
Raisess <raisechif@gmail.com>
2021-03-06 14:57:29 -0300

    added subject default message

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const emailList: Array<StringCouple> = parser.getEmailList();
 for (const item of emailList) {
 	setTimeout((): void => {
 		email.send(item[1],
-		           process.env.SUBJECT! || "",
+		           process.env.SUBJECT! || "Ping",
 		           process.env.MESSAGE! || `Pong! Oi ${item[0]}, ignore esse email!`,
 		           !!process.env.HTML!);
 	}, parseFloat(process.env.DELAY!) || 2000);
